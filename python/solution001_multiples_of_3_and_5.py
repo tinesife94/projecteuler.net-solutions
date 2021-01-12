2547ed64c1a7ed98d2c0dd83dd87a86cf5f68247
Tinesife <tinesife94@gmail.com>
2021-01-12 15:12:25 -0300

    Code refactoring: Solution 1

--- python/solution001_multiples_of_3_and_5.py
+++ python/solution001_multiples_of_3_and_5.py
@@ -4,11 +4,25 @@ at:
 https://projecteuler.net/problem=1
 
 For your convinience:
-
+----------------------------------------------------------------------------
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we
 get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
+----------------------------------------------------------------------------
+Usage:
+
+python solution001_multiples_of_3_and_5.py [multiples] [limit]
+
+Exemples:
+
+python solution001_multiples_of_3_and_5.py 3 5 1000
+
+python solution001_multiples_of_3_and_5.py 1000
+
+python solution001_multiples_of_3_and_5.py 3 5
+
+python solution001_multiples_of_3_and_5.py
 """
 
 def sum_terms_arithmetic_progression(start: int, stop: int, step: int) -> int:
@@ -17,9 +31,10 @@ def sum_terms_arithmetic_progression(start: int, stop: int, step: int) -> int:
     An efficient way to get the sum of all multiples of a number up to a
     certain stop.
     """
-    if not step:
+    try:
+        last = stop - stop % step
+    except ZeroDivisionError:
         return start
-    last = stop - stop % step
     if stop == last:
         last -= step
     num_of_terms = ((last - start) // step) + 1
@@ -27,19 +42,25 @@ def sum_terms_arithmetic_progression(start: int, stop: int, step: int) -> int:
 
 def sum_all_multiples_of_x_or_y_below_lim(x_value: int, y_value: int,
                                           lim: int) -> int:
-    """Function that solves problem 1."""
+    """Find the sum of all the multiples of x_value or y_value below lim."""
     x_times_y = x_value * y_value
     return (sum_terms_arithmetic_progression(x_value, lim, x_value)
             + sum_terms_arithmetic_progression(y_value, lim, y_value)
             - sum_terms_arithmetic_progression(x_times_y, lim, x_times_y))
 
-if __name__ == "__main__":
-    import sys
-    NUM_ARGS = len(sys.argv)
-    if NUM_ARGS == 4:
-        x_arg, y_arg, lim_arg = [int(arg) for arg in sys.argv[1:]]
-    elif NUM_ARGS == 2:
-        x_arg, y_arg, lim_arg = 3, 5, int(sys.argv[1])
+def _solution(argv : []) -> None:
+    num_args = len(argv)
+    if num_args == 4:
+        x_arg, y_arg, lim_arg = [int(arg) for arg in argv[1:]]
+    elif num_args == 3:
+        x_arg, y_arg = [int(arg) for arg in argv[1:]]
+        lim_arg = 1_000
+    elif num_args == 2:
+        x_arg, y_arg, lim_arg = 3, 5, int(argv[1])
     else:
         x_arg, y_arg, lim_arg = 3, 5, 1_000
     print(sum_all_multiples_of_x_or_y_below_lim(x_arg, y_arg, lim_arg))
+
+if __name__ == "__main__":
+    import sys
+    _solution(sys.argv)
